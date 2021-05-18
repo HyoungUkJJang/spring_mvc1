5088f15c8f1f91af747bd0198866502b64840666
HyoungUk <dnr759@naver.com>
2021-05-18 21:32:30 +0900

    서블릿 학습

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 스프링 부트에서 서블릿을 사용할 수 있도록 지원하는 애노테이션
 @SpringBootApplication
 public class ServletApplication {
 
